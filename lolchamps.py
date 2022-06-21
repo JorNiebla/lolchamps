2181fbac13d08d8ef76ceb58d5cd59d67aae7372
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2022-06-22 00:17:24 +0100

    Fix typo

--- lolchamps.py
+++ lolchamps.py
@@ -14,8 +14,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 con = psycopg2.connect(DATABASE_URL)
 cur = con.cursor()
 
-lanes = {'top': "TOP", 'jg': "JGL", 'jung': "JGL", 'jng': "JGL",'jungle': "JHL", 'jungler': "JGL", 'mid': "MID", 'middle' : "MID", 'adc': "ADC", 'bot': "ADC", 'supp': "ADC", 'sup': "ADC"}
-
+lanes = {'top': "TOP", 'jg': "JGL", 'jung': "JGL", 'jng': "JGL",'jungle': "JGL", 'jungler': "JGL", 'mid': "MID", 'middle' : "MID", 'adc': "ADC", 'bot': "ADC", 'supp': "ADC", 'sup': "ADC"}
 
 def generate_embed(champ,lane):
     embedVar = discord.Embed(color=0x00ff00)
