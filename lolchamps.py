460172bb6d08eef53821d35dd4e8061cdd66c533
Jorge Niebla Núñez <alu0101215457@ull.edu.es>
2022-06-28 10:32:00 +0100

    Mejora en la consulta para sacar stats y limpieza de codigo

--- lolchamps.py
+++ lolchamps.py
@@ -33,7 +33,7 @@ def random_champ(lane,userid,cur):
     champ = random.choice(cur.fetchall())
     return champ
 
-async def win_champ(cur,con,champname,message):
+async def win_champ(cur,con,champname,userid,channel):
     champid=MAXINT
     cur.execute("SELECT CHAMP, CHAMPID FROM champions")
     champlist = cur.fetchall()
@@ -43,16 +43,16 @@ async def win_champ(cur,con,champname,message):
             break
 
     if champid == MAXINT:
-        await message.channel.send("I dont know that champion, please type a valid champion", delete_after=10)
+        await channel.send("I dont know that champion, please type a valid champion", delete_after=10)
     else:
         try:
             cur.execute(f"""INSERT INTO wins (CHAMPID, PLAYERID)
-                        VALUES({champid}, {message.author.id})""")
+                        VALUES({champid}, {userid})""")
             con.commit()
-            await message.channel.send(f"Congratulations on the win with {champname}", delete_after=10)
+            await channel.send(f"Congratulations on the win with {champname}", delete_after=10)
         except:
             print(traceback.format_exc())
-            await message.channel.send(f"Something went wrong, sorry I couldn't register the win with {champname}", delete_after=10) 
+            await channel.send(f"Something went wrong, sorry I couldn't register the win with {champname}", delete_after=10) 
 
 async def printlaner(lane,userid,champmsg,interaction,components,cur):
     champ = random_champ(lanes[lane],userid,cur)
@@ -70,6 +70,7 @@ class MyClient(discord.Client):
         cur = con.cursor()
         pid = random.randint(MININT,MAXINT)
         userid = message.author.id
+        channel = message.channel
 
         botpinged = False
         for role in message.role_mentions:
@@ -86,50 +87,39 @@ class MyClient(discord.Client):
             pass
 
         elif("update" in message.content) and (userid == 371076929022984196):
-            await message.channel.send("Updating database...", delete_after=10)
+            await channel.send("Updating database...", delete_after=10)
             db.update_champions_DB(con,cur)
-            await message.channel.send("Database updated!", delete_after=10)
+            await channel.send("Database updated!", delete_after=10)
             await message.delete(delay=10)
 
-        elif(f"stats" in message.content):
+        elif("stats" in message.content):
             try:
-                nowinsandwins = []
                 lane = message.clean_content.replace(f"@{self.user.name} stats ", '')
                 if lane in lanes:
-                    cur.execute(f"""SELECT COUNT(*) 
-                                    FROM champions 
-                                    WHERE {lanes[lane]} = True AND CHAMPID NOT IN
+                    cur.execute(f"""SELECT 
+                                    sum(case when {lanes[lane]} = True AND CHAMPID NOT IN
                                         (SELECT CHAMPID
                                         FROM wins
-                                        WHERE PLAYERID = '{userid}'
-                                        )""")
-                    nowinsandwins.append(cur.fetchall()[0][0])
-                    cur.execute(f"""SELECT COUNT(*)
-                                    FROM champions 
-                                    WHERE {lanes[lane]} = True AND CHAMPID IN
+                                        WHERE PLAYERID = '{userid}') then 1 else 0 end),
+                                    sum(case when {lanes[lane]} = True AND CHAMPID IN
                                         (SELECT CHAMPID
                                         FROM wins
-                                        WHERE PLAYERID = '{userid}'
-                                        )""")
-                    nowinsandwins.append(cur.fetchall()[0][0])
+                                        WHERE PLAYERID = '{userid}') then 1 else 0 end)
+                                    FROM champions""")
+                    nowinsandwins = (cur.fetchall()[0])
                     statsfor = lanes[lane]
                 else:
-                    cur.execute(f"""SELECT COUNT(*) 
-                                    FROM champions 
-                                    WHERE CHAMPID NOT IN
+                    cur.execute(f"""SELECT 
+                                    sum(case when CHAMPID NOT IN
                                         (SELECT CHAMPID
                                         FROM wins
-                                        WHERE PLAYERID = '{userid}'
-                                        )""")
-                    nowinsandwins.append(cur.fetchall()[0][0])
-                    cur.execute(f"""SELECT COUNT(*)
-                                    FROM champions 
-                                    WHERE CHAMPID IN
+                                        WHERE PLAYERID = '{userid}') then 1 else 0 end),
+                                    sum(case when CHAMPID IN
                                         (SELECT CHAMPID
                                         FROM wins
-                                        WHERE PLAYERID = '{userid}'
-                                        )""")
-                    nowinsandwins.append(cur.fetchall()[0][0])
+                                        WHERE PLAYERID = '{userid}') then 1 else 0 end)
+                                    FROM champions""")
+                    nowinsandwins = (cur.fetchall()[0])
                     statsfor = "Total"
 
                 colors = ["#EB5757", "#5AC91A"]
@@ -151,23 +141,23 @@ class MyClient(discord.Client):
                 embed = discord.Embed(title=f"Stats for {message.author.name} in {statsfor}",color=0x00ff00) #creates embed
                 piefile = discord.File(f'temp{pid}.png', filename="grahp.png")
                 embed.set_image(url="attachment://graph.png")
-                await message.channel.send(file=piefile, embed=embed)
+                await channel.send(file=piefile, embed=embed)
                 os.remove(f'temp{pid}.png')
 
             except psycopg2.errors.UndefinedTable:
-                await message.channel.send("You don't have a profile, create one by mentioning me and typing 'new'", delete_after=10)
+                await channel.send("You don't have a profile, create one by mentioning me and typing 'new'", delete_after=10)
             except:
                 print(traceback.format_exc())
-                await message.channel.send("Something went wrong, sorry I couldn't get your stats", delete_after=10) 
+                await channel.send("Something went wrong, sorry I couldn't get your stats", delete_after=10) 
         
-        elif(f"win" in message.content):       
+        elif("win" in message.content):
             champs = message.clean_content.split("win", 1)[1].replace("'", "''").strip()
             if champs=='':
-                await message.channel.send("You didn't type a champion, please type a valid champion", delete_after=10)
+                await channel.send("You didn't type a champion, please type a valid champion", delete_after=10)
             else:
                 champlist = champs.split(",")
                 for champname in champlist:
-                    await win_champ(cur,con,champname.strip(),message)
+                    await win_champ(cur,con,champname.strip(),userid,channel)
 
             await message.delete(delay=10)
 
@@ -232,7 +222,7 @@ class MyClient(discord.Client):
                         case 8: #Button for selecting support
                             lane = "supp"
                         case 9: #Confirm win
-                            await win_champ(cur,con,champ[0],message)
+                            await win_champ(cur,con,champ[0],userid,channel)
                         case 10: #Cancel win
                             continue
                     champ = random_champ(lanes[lane],userid,cur)
@@ -240,13 +230,13 @@ class MyClient(discord.Client):
                     await champmsg.edit('',embed=embedVar, **components)
                     await interaction.send(content=f"<a:kirby:759485375718359081>Re-Roll {lane}<a:kirby:759485375718359081>",ephemeral=False, delete_after=0.1)
                 except asyncio.TimeoutError:
-                    await message.channel.send("Timeout, deleting message...", delete_after=10)
+                    await channel.send("Timeout, deleting message...", delete_after=10)
                     await champmsg.delete()
                     await message.delete()
                     break
                 except:
                     print(traceback.format_exc())
-                    await message.channel.send("Exception", delete_after=10)
+                    await channel.send("Exception", delete_after=10)
                     await champmsg.delete()
                     await message.delete()
                     break
